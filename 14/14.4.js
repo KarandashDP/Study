7aa4c741a6f98670e8cd3d0a8180a6daccace22b
Stanislav <64573528+KarandashDP@users.noreply.github.com>
2020-05-11 12:48:09 +0300

    Update 14.4.js

--- 14/14.4.js
+++ 14/14.4.js
@@ -1,6 +1,11 @@
-const request = require('request');
+const div = document.querySelector('div');
 
-request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3', { json: true }, (err, res, body) => {
-    if (err) { return console.log(err); }
-    console.log(body);
-});
+const req = new XMLHttpRequest();
+
+req.open('GET', "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3");
+// req.responseType = 'json';
+req.onload = function () {
+        div.textContent = req.response;
+}
+
+req.send();
